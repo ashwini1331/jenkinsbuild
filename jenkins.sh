2e6ec8ae80efb01830c65ee5815ac3331168ed7f
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-08-21 17:13:34 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo yum install -y jenkins java-11-openjdk-devel
+sudo yum update -y 
 sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install java-11-openjdk
 sudo yum upgrade
 sudo yum install jenkins
 
