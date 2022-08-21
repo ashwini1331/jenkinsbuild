85c2bb9ab1f2301cc12d9502eb6f6e0d58461e7e
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-08-21 19:35:45 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,4 +5,4 @@ sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key ad
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update 
-sudo apt install -y jenkins 
+sudo apt install jenkins 
