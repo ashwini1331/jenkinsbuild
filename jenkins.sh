c91da8400e4a474bddee05626ccdd4df1365368b
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-08-21 18:17:22 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,7 @@ sudo apt install openjdk-8-jdk
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo killall apt apt-get
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock*
