9e36848d88aeabaed972fe8110ad59e3a5d3a74f
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-09-22 21:19:00 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo apt install -y openjdk-8-jdk 
-sudo apt install -y openjdk-8-jre
-sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo apt update && sudo apt upgrade -y
+sudo apt install default-jdk -y
+sudo mkdir -p /usr/share/keyrings
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y jenkins 
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl status jenkins 
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
